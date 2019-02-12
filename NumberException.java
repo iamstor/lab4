9211ff96d87d24fb5e1eb03a1bd63615f6de0453
iamstor <45497066+iamstor@users.noreply.github.com>
2019-02-12 22:20:24 +0300

    Create NumberException.java

---
+++ NumberException.java
@@ -0,0 +1,14 @@
+public class NumberException extends IllegalArgumentException{
+ int num;
+    public NumberException(int num) {
+        this.num=num;
+    }
+
+    @Override
+    public String getMessage(){
+        return "Неверное количество  " + num;
+    }
+}
+
+
+
