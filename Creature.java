5ad75a771cabe3913cce215e3e205c6c206c895b
iamstor <45497066+iamstor@users.noreply.github.com>
2019-02-12 22:18:39 +0300

    Create Creature.java

---
+++ Creature.java
@@ -0,0 +1,19 @@
+
+
+public abstract class Creature {
+    String name;
+
+
+    Creature() {
+    }
+
+    public Creature(String name) {
+
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+}
